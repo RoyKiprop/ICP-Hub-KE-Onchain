f951fb1328308099a1dc93f6e0e3a8573fee0b8e
Roy Kiprop <roykiprop590@gmail.com>
2024-10-14 21:03:43 +0300

    Modify footer

--- frontend/components/organisms/Footer/index.tsx
+++ frontend/components/organisms/Footer/index.tsx
@@ -5,30 +5,53 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="pb-40 pt-16 bg-[#632020] flex justify-center">
-      <div className="flex flex-col items-center gap-8 lg:flex-row lg:gap-16 xl:gap-20">
-        <div className="space-y-5 text-center">
-          <NavBrand />
-          <div className="">
-            <Text
-              value={`Copyright © ${new Date().getFullYear()}`}
-              textStyle="SectionParagraph"
-            />
-            <Text value={``} textStyle="SectionParagraph" />
+    <footer className="bg-[#632020] text-white py-16 px-16 flex flex-col ">
+      <div className="container flex flex-col space-y-5">
+        <div className="flex justify-between">
+          <div className="flex flex-col items-center lg:items-start text-center lg:text-left mb-12 lg:mb-0">
+            <h2 className="text-4xl font-bold mb-4">Join The Revolution</h2>
+            <p className="mb-4 max-w-sm">
+              ICP Hub Kenya is dedicated to providing the advancement of
+              blockchain technology within our region and beyond.
+            </p>
+            <a
+              href="#"
+              className="bg-[#f6b73c] text-black px-6 py-3 rounded-lg hover:bg-[#d9972c] transition"
+            >
+              Join ICP Events
+            </a>
+          </div>
+          <div className="mt-12 lg:mt-0">
+            <img src="" alt="Astronaut" className="w-64 h-auto" />
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:gap-16 xl:gap-20 text-center">
-          <div className="space-y-5">
-            <Text value="Follow us" textStyle="FooterLinkGroupTitle" />
-            <div className="space-y-[10px]">
-              <NavLink 
-                value="Telegram" 
-                href="https://t.me/icphub_KE" 
-              />
-              <NavLink 
-                value="Twitter" 
-                href="https://twitter.com/icphub_KE" 
-              />
+
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+          <div>
+            <NavBrand />
+            <p className="text-sm mt-4">
+              Copyright © {new Date().getFullYear()}
+            </p>
+          </div>
+          <div>
+            <h4
+              className="text-lg font-bold 
+Follow Us
+Telegram
+
+Twitter
+
+Instagram
+
+LinkedIn
+
+mb-4"
+            >
+              Follow Us
+            </h4>
+            <ul className="space-y-2">
+              <NavLink value="Telegram" href="https://t.me/icphub_KE" />
+              <NavLink value="Twitter" href="https://twitter.com/icphub_KE" />
               <NavLink
                 value="Instagram"
                 href="https://www.instagram.com/icphub_ke/"
@@ -37,23 +60,22 @@ const Footer = () => {
                 value="LinkedIn"
                 href="https://www.linkedin.com/company/icphub-ke/"
               />
-            </div>
+            </ul>
           </div>
-          <div className="space-y-5">
-            <Text value="Education" textStyle="FooterLinkGroupTitle" />
-            <div className="space-y-[10px]">
+          <div>
+            <h4 className="text-lg font-bold mb-4">Education</h4>
+            <ul className="space-y-2">
               <NavLink
                 value="Dacade course"
                 href="https://dacade.org/communities/icp"
               />
-            
-            </div>
+            </ul>
           </div>
-          <div className="space-y-5">
-            <Text value="Blog" textStyle="FooterLinkGroupTitle" />
-            <div className="space-y-[10px]">
+          <div>
+            <h4 className="text-lg  font-bold mb-4">Blog</h4>
+            <ul className="space-y-2">
               <NavLink value="Our blog posts" href="/blog" />
-            </div>
+            </ul>
           </div>
         </div>
       </div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,151 +1,231 @@
-import React from 'react'
-import { FiFigma, FiCode, FiBox, FiDollarSign, FiAirplay } from 'react-icons/fi'
+import React from "react";
+import {
+  FiFigma,
+  FiCode,
+  FiBox,
+  FiDollarSign,
+  FiAirplay,
+} from "react-icons/fi";
 
-import ButtonLink from '../components/atoms/Button/ButtonLink'
-import LineDivider from '../components/atoms/LineDivider'
-import FeatureCard from '../components/molecules/Card/FeatureCard'
-import PageSentence from '../components/molecules/PageSentence'
-import PricingCard from '../components/molecules/Card/PricingCard'
-import ProjectCard from '../components/molecules/Card/ProjectCard'
-import SectionSentence from '../components/molecules/SectionSentence'
-import LogoList from '../components/organisms/LogoList'
-import TestimonialList from '../components/organisms/TestimonialList'
-import PageTemplate from '../components/templates/PageTemplate'
-import Image from 'next/image'
-import TeamCard from '../components/molecules/Card/TeamCard'
-import CallOut from '../components/organisms/CallOut'
-import Footer from '../components/organisms/Footer'
-import Link from 'next/link'
-import TechJourneyStats from '../components/molecules/Card/JourneyStarts'
-import GrantApplicationCard from '../components/molecules/Card/GrantApplicationCard/GrantApplicationCard'
-import ICPEducationCard from '../components/molecules/Card/ICPEducationCard/ICPEducationCard'
-import InnovationIncubatorCard from '../components/molecules/Card/InnovatorCard/InnovatorCard'
-import VisionVideoCard from '../components/molecules/Card/VisionCard/VisionCard'
+import ButtonLink from "../components/atoms/Button/ButtonLink";
+import LineDivider from "../components/atoms/LineDivider";
+import FeatureCard from "../components/molecules/Card/FeatureCard";
+import PageSentence from "../components/molecules/PageSentence";
+import PricingCard from "../components/molecules/Card/PricingCard";
+import ProjectCard from "../components/molecules/Card/ProjectCard";
+import SectionSentence from "../components/molecules/SectionSentence";
+import LogoList from "../components/organisms/LogoList";
+import TestimonialList from "../components/organisms/TestimonialList";
+import PageTemplate from "../components/templates/PageTemplate";
+import Image from "next/image";
+import TeamCard from "../components/molecules/Card/TeamCard";
+import CallOut from "../components/organisms/CallOut";
+import Footer from "../components/organisms/Footer";
+import Link from "next/link";
+import TechJourneyStats from "../components/molecules/Card/JourneyStarts";
+import GrantApplicationCard from "../components/molecules/Card/GrantApplicationCard/GrantApplicationCard";
+import ICPEducationCard from "../components/molecules/Card/ICPEducationCard/ICPEducationCard";
+import InnovationIncubatorCard from "../components/molecules/Card/InnovatorCard/InnovatorCard";
+import VisionVideoCard from "../components/molecules/Card/VisionCard/VisionCard";
 
 const statsData = [
-  { value: '20+', label: 'Hackathon' },
-  { value: '1k+', label: 'Developers' },
-  { value: '10+', label: 'Startups' },
+  { value: "20+", label: "Hackathon" },
+  { value: "1k+", label: "Developers" },
+  { value: "10+", label: "Startups" },
 ];
 
 const Home = () => {
   return (
     <>
-
-        <PageTemplate title="Home - ICP HUB KE">
-
+      <PageTemplate title="Home - ICP HUB KE">
         {/* Banner Section */}
         <section className="relative overflow-hidden w-full h-[80vh] p-[60px_40px] mt-[30px] bg-gradient-to-r from-[#571E1E] to-[#340A0A] rounded-[60px] shadow-xl border-[6px] border-[#400d0b] hero-section">
           <div className="flex justify-between items-center max-w-[1200px] mx-auto">
             <div className="flex-1 text-white pr-10 text-base">
-              <h1 className="text-5xl font-bold mb-5">Revolutionizing Blockchain Education</h1>
+              <h1 className="text-5xl font-bold mb-5">
+                Revolutionizing Blockchain Education
+              </h1>
               <p className="text-lg mb-8">
-                At the forefront of technology is ICP.Hub Kenya dedicated to advancing the Internet Computer ecosystem.
+                At the forefront of technology is ICP.Hub Kenya dedicated to
+                advancing the Internet Computer ecosystem.
               </p>
               <button className="bg-[#e0a040] text-black px-6 py-3 rounded font-bold cursor-pointer transition-colors duration-300 hover:bg-[#d09030]">
                 Explore Kenya ICP Hub
               </button>
             </div>
             <div className="flex-1 relative">
-              <img src="/images/HeroImage.svg" alt="ICP Hub Kenya Group" className="w-full h-full object-cover rounded-[30px]" />
+              <img
+                src="/images/HeroImage.svg"
+                alt="ICP Hub Kenya Group"
+                className="w-full h-full object-cover rounded-[30px]"
+              />
               {/* <Image src={'/images/HeroImage.svg'} width={200} height={100} alt='Grant Application' loading='lazy' /> */}
             </div>
           </div>
         </section>
 
-
         {/* Feature List */}
         <section className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-16">
-  
           <div className="col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
-            <TechJourneyStats 
-              title="We provide easy medium to get started in your tech journey" 
-              stats={statsData} 
+            <TechJourneyStats
+              title="We provide easy medium to get started in your tech journey"
+              stats={statsData}
             />
-            <GrantApplicationCard /> 
-          </div>
-          
-          <div className="col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
-            <ICPEducationCard /> 
-            <InnovationIncubatorCard /> 
+            <GrantApplicationCard />
           </div>
 
+          <div className="col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <ICPEducationCard />
+            <InnovationIncubatorCard />
+          </div>
         </section>
 
         {/* Vision Card */}
-          <VisionVideoCard />
+        <VisionVideoCard />
 
         {/* Our Teams */}
-        <section className="grid grid-cols-1 gap-6 sm:gap-8 place-items-center lg:grid-cols-2 px-4 sm:px-8 md:px-12 lg:px-0">
-          <aside className="text-center sm:w-10/12 lg:text-left lg:w-full">
-            <div className="space-y-12" data-aos="">
-              <SectionSentence
-                title="Working behind the scenes"
-                paragraph="Our team is driven by a shared commitment to empowering individuals and businesses with the transformative potential of this groundbreaking technology."
-                badge="OUR TEAM"
-              />
-              <Link href="/about" passHref>
-                <ButtonLink
-                  value="See Our Team"
-                  size="small"
-                  color="white"
-                  style="light"
-                  href="/about"
+        <section className="bg-[#4b1c1c] border-8 py-12 px-4 sm:px-8 md:px-12 w-full">
+          <h2 className="text-white text-4xl font-bold text-center mb-12">
+            Our Core Team
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+            {/* Team member 1 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/yvonne.png"
+                  alt="Yvonne Kagondu"
+                  className="w-full h-full object-cover"
                 />
-              </Link>
+              </div>
+              <h3 className="text-white text-lg font-semibold">
+                Yvonne Kagondu
+              </h3>
+              <p className="text-gray-300">CO-FOUNDER</p>
             </div>
-          </aside>
-          <aside
-            className="w-full h-[400px] relative sm:w-8/12 lg:w-full px-4 sm:px-8 md:px-12 lg:px-0"
-            data-aos=""
-          >
-            <div className="w-full grid grid-cols-3 gap-8 mt-4 overflow-x-auto">
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/yvonne.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/tevin.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/Mary.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/steve.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/bella.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/sammy.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/maic.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/orlah.png" alt="" />
-              </div>
-              <div className="w-32 h-32 shrink-0">
-                <img src="/images/charles.png" alt="" />
-              </div>
-            </div>
-          </aside>
 
+            {/* Team member 2 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/tevin.png"
+                  alt="Tevin Isaac"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">Tevin Isaac</h3>
+              <p className="text-gray-300">DEVELOPER</p>
+            </div>
+
+            {/* Team member 3 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/Mary.png"
+                  alt="Mary Usaji"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">Mary Usaji</h3>
+              <p className="text-gray-300">PROJECT MANAGER</p>
+            </div>
+            {/* Team member 4 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/steve.png"
+                  alt="Steve KImoi"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">Steve Kimoi</h3>
+              <p className="text-gray-300">PROJECT MANAGER</p>
+            </div>
+            {/* Team member 5 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/bella.png"
+                  alt="Bella Chris"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">Bella Chris</h3>
+              <p className="text-gray-300">COMMUNITY MANAGER</p>
+            </div>
+            {/* Team member 6 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/sammy.png"
+                  alt="Sammy Fash"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">Sammy Fash</h3>
+              <p className="text-gray-300">LEAD DESIGNER</p>
+            </div>
+            {/* Team member 7 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-4 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/orlah.png"
+                  alt="Peace Adekoya"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">
+                Peace Adekoya
+              </h3>
+              <p className="text-gray-300">SOCIAL MEDIA MANAGER</p>
+            </div>
+            {/* Team member 8 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64   mb-4 overflow-hidden">
+                <img
+                  src="/images/maic.png"
+                  alt="Maic Sebakara"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">
+                Maic Sebakara
+              </h3>
+              <p className="text-gray-300">ICP RWANDA SPOKE</p>
+            </div>
+            {/* Team member 9 */}
+            <div className="flex flex-col items-center">
+              <div className="w-64 h-64 border-8 border-[#f8b230] rounded-md mb-4 overflow-hidden">
+                <img
+                  src="/images/charles.png"
+                  alt="Charles Maina"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h3 className="text-white text-lg font-semibold">
+                Charles Maina
+              </h3>
+              <p className="text-gray-300">TECHNICAL WRITER</p>
+            </div>
+          </div>
         </section>
-        <LineDivider />
-       
-      </PageTemplate>
 
+        <LineDivider />
+      </PageTemplate>
 
       {/* Section with light background */}
       <PageTemplate title="Home - ICP HUB KE" light>
-        <section data-light-section className="flex flex-col gap-16 items-center mt-0">
-          <div 
-            className="text-center sm:w-10/12 md:w-8/12 lg:w-6/12 mt-0 mb-2 sm:mb-0" 
+        <section
+          data-light-section
+          className="flex flex-col gap-16 items-center mt-0"
+        >
+          <div
+            className="text-center sm:w-10/12 md:w-8/12 lg:w-6/12 mt-0 mb-2 sm:mb-0"
             data-aos=""
           >
-            <SectionSentence 
-              lightTitle="ICP has unique features" 
-              lightBadge="WHAT ICP OFFERS TODAY" 
+            <SectionSentence
+              lightTitle="ICP has unique features"
+              lightBadge="WHAT ICP OFFERS TODAY"
             />
           </div>
           <div className="w-full grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-1 mt-2 sm:mt-0">
@@ -189,15 +269,11 @@ const Home = () => {
         <div data-aos="">
           <CallOut />
         </div>
-
       </PageTemplate>
 
-      <Footer /> 
-
-
-      
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
