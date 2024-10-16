07e60362df61840d262ccaf8e350d11fc9e7c7fc
Roy Kiprop <roykiprop590@gmail.com>
2024-10-16 08:27:15 +0300

    modify teams section

--- frontend/components/organisms/Footer/index.tsx
+++ frontend/components/organisms/Footer/index.tsx
@@ -8,24 +8,24 @@ const Footer = () => {
     <footer className="bg-[#632020] text-white py-16 px-24 flex flex-col ">
       <div className="container flex flex-col space-y-5">
         <div className="flex items-center justify-between">
-          <div className="flex flex-col items-center lg:items-start text-center lg:text-left mb-12 lg:mb-0">
-            <h2 className="text-4xl font-bold mb-4">Join The Revolution</h2>
+          <div className="flex flex-col space-y-4 items-center lg:items-start text-center lg:text-left mb-12 lg:mb-0">
+            <h2 className="text-6xl font-bold mb-4">Join The Revolution</h2>
             <p className="mb-4 max-w-sm">
               ICP Hub Kenya is dedicated to providing the advancement of
               blockchain technology within our region and beyond.
             </p>
             <a
               href="#"
-              className="bg-[#f6b73c] text-black px-6 py-3 rounded-lg hover:bg-[#d9972c] transition"
+              className="bg-[#f6b73c] text-black text-xl  px-6 py-3 w-[50%] rounded-lg hover:bg-[#d9972c] transition"
             >
-              Join ICP Events
+              Join ICP Kenya Hub
             </a>
           </div>
           <div className="mt-12 lg:mt-0">
             <img
               src="/images/Astronaut.svg"
               alt="Astronaut"
-              className="max-w-3xl h-auto"
+              className="max-w-4xl h-auto"
             />
           </div>
         </div>
@@ -38,22 +38,8 @@ const Footer = () => {
             </p>
           </div>
           <div>
-            <h4
-              className="text-lg font-bold 
-Follow Us
-Telegram
-
-Twitter
-
-Instagram
-
-LinkedIn
-
-mb-4"
-            >
-              Follow Us
-            </h4>
-            <ul className="space-y-2">
+            <h4 className="text-lg font-bold">Follow Us</h4>
+            <ul className="space-y-4">
               <NavLink value="Telegram" href="https://t.me/icphub_KE" />
               <NavLink value="Twitter" href="https://twitter.com/icphub_KE" />
               <NavLink
