1d20d8a81e2cdffee47eb7f4f1459cbd1bd5b40c
Stephen-Kimoi <moenisteve@gmail.com>
2024-10-16 14:15:02 +0300

    finalizing on nav bar

--- frontend/components/organisms/NavBar/index.tsx
+++ frontend/components/organisms/NavBar/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import NavBrand from "../../atoms/NavBrand";
 import NavLink from "../../atoms/NavLink";
-import ButtonLink from "../../atoms/Button/ButtonLink";
 import Container from "../../templates/Container";
 import { FiMenu, FiX } from "react-icons/fi";
 import useMobileDeviceDetection from "../../../hooks/useMobileDetection";
@@ -48,70 +47,48 @@ const NavBar = () => {
   }, [isOpen]);
 
   return (
-    <>
-      {!isScrolled && (
-        <nav
-          className={`pt-8 xl:px-[150px] w-full top-0 left-0 z-30 ${
-            isScrolled ? "fixed" : "absolute"
-          }`}
-        >
-          <Container>
-            <div
-              className={`px-6 py-4 w-full rounded-md ${
-                isScrolled || isOpen ? "" : "lg:px-0"
-              }`}
-            >
-              <div className="flex flex-row items-center w-full justify-between lg:place-items-center">
-                <NavBrand />
+    <nav className={`xl:px-[150px] w-full top-5 left-0 z-30 ${isScrolled ? "fixed" : "absolute"}`}>
+      <Container>
+        <div className="flex items-center justify-between py-4">
+          <NavBrand />
+          
+          {/* Desktop menu */}
+          <div className="hidden lg:flex items-center space-x-8">
+            <NavLink href="/about" value="About" canActive={true} />
+            <NavLink href="/education" value="Education" canActive={true} />
+            <NavLink href="/events" value="Events" canActive={true} />
+            <NavLink href="/blog" value="Blog" canActive={true} />
+            <NavLink href="/contact" value="Contact" canActive={true} />
+            <GradientButton onClick={handleTelegramRedirect}>
+              <span className="px-4">Join Us</span>
+            </GradientButton>
+          </div>
 
-                {/* Hamburger menu icon for mobile */}
-                <div className="lg:hidden ml-auto" onClick={() => setIsOpen(!isOpen)}>
-                  <div
-                    className={`text-black text-2xl ${
-                      isMobile ? "cursor-default" : "cursor-pointer"
-                    }`}
-                  >
-                    {isOpen ? <FiX /> : <FiMenu />}
-                  </div>
-                </div>
-
-                {/* Mobile dropdown menu */}
-                <div
-                  className={`${
-                    isOpen ? "absolute top-12 right-4 bg-white p-4 shadow-lg rounded-lg" : "hidden"
-                  } lg:flex lg:items-center lg:space-x-8`}
-                >
-                  {/* Close button for mobile menu */}
-                  <div
-                    className="absolute top-2 right-2 text-black text-2xl cursor-pointer"
-                    onClick={() => setIsOpen(false)}
-                  >
-                    <FiX />
-                  </div>
-
-                  <div className="flex flex-col space-y-5">
-                    <NavLink href="/education" value="Education" canActive={true} />
-                    <NavLink href="/events" value="Events" canActive={true} />
-                    <NavLink href="/blog" value="Blog" canActive={true} />
-                    <NavLink href="/about" value="About" canActive={true} />
-                    <NavLink href="/partners" value="Partners" canActive={true} />
-                  </div>
-
-                  <div className="mt-5">
-                    <GradientButton onClick={handleTelegramRedirect}>
-                      <span className="px-4">Join Us</span>
-                    </GradientButton>
-                  </div>
-                </div>
-              </div>
-
-
-
+          {/* Mobile menu icon */}
+          <div className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+            <div className={`text-black text-2xl ${isMobile ? "cursor-default" : "cursor-pointer"}`}>
+              {isOpen ? <FiX /> : <FiMenu />}
             </div>
-          </Container>
-        </nav>
+          </div>
+        </div>
+      </Container>
+
+      {/* Mobile dropdown menu */}
+      {isOpen && (
+        <div className="lg:hidden absolute top-16 left-0 right-0 bg-white p-4 shadow-lg">
+          <div className="flex flex-col space-y-5">
+            <NavLink href="/about" value="About" canActive={true} />
+            <NavLink href="/education" value="Education" canActive={true} />
+            <NavLink href="/events" value="Events" canActive={true} />
+            <NavLink href="/blog" value="Blog" canActive={true} />
+            <NavLink href="/contact" value="Contact" canActive={true} />
+            <GradientButton onClick={handleTelegramRedirect}>
+              <span className="px-4">Join Us</span>
+            </GradientButton>
+          </div>
+        </div>
       )}
-    </>
+    </nav>
   );
 };
 
